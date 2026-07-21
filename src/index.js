2d3f3d19923801dc6fa6b7a22408e2b8308c0e9d
Erin Wilson <ewilson001500@gmail.com>
2026-07-21 16:49:07 -0500

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ function generatorPoem(event) {
     "You love to write short poems. Your mission is to generator 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. DO NOT include a title to the poem. Do Not display the HTML. Sign  the poe with 'AI Generated' inside a <strong> element at the end of the poem and NOT at the beginning.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+
+  poemElement.innerHTML = `<div class="loading">🧠</div> Generating a poem about ${promptInput.value}`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
